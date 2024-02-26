78ef52576b2c85b1f6965b18cb6bb77f4be645d3
Santhosh S <officialuse3012@gmail.com>
2024-02-26 20:05:02 +0530

    ngGor directive used to display the rooms

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Room } from './rooms';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -19,6 +19,36 @@ export class RoomsComponent implements OnInit {
     availableRooms: 8,
     bookedRooms: 12
   };
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 101,
+      roomType: "Deluxe",
+      amenities: "AC, TV, WiFi, Breakfast",
+      price: 5000,
+      photos: "https://www.parkregisgoa.in/wp-content/uploads/2021/04/Deluxe-Garden-View-2-2520x1400.jpg",
+      checkInTime: new Date('2024-02-14T20:00:17'),
+      checkOutTime: new Date('2024-02-15T06:16:48')
+    },
+    {
+      roomNumber: 102,
+      roomType: "Standard",
+      amenities: "TV, WiFi, Breakfast",
+      price: 3000,
+      photos: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SIyWC7gKJIM_rPMXS-LbY5X2V4C8zXXfqw&usqp=CAU",
+      checkInTime: new Date('2024-01-30T18:00:17'),
+      checkOutTime: new Date('2024-01-31T08:16:48')
+    },
+    {
+      roomNumber: 103,
+      roomType: "Private Suite",
+      amenities: "AC, TV, WiFi, Breakfast, Dinner",
+      price: 8000,
+      photos: "https://www.parkregisgoa.in/wp-content/uploads/2021/04/Suite-Sea-View-2-2520x1400.jpg",
+      checkInTime: new Date('2024-03-20T22:00:17'),
+      checkOutTime: new Date('2024-03-21T10:16:48')
+    }
+  ];
 
   constructor() { }
 
